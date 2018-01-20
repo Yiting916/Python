0433bf4d2fba7f6f1887550b497087d294933030
Summer <yiting.chen916@gmail.com>
2018-01-20 21:03:27 +0800

    HW2_prime.py

---
+++ pyt292103_prime_hw2.py
@@ -0,0 +1,10 @@
+for n in range(2, 101):
+    fct = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            fct += 1
+
+    if fct == 2:
+        print(n, 'is prime')
+    else:
+        print(n, 'is not prime')
