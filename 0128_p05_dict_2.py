9789207635847749d08a4e467dfa1ae911628a13
Summer <yiting.chen916@gmail.com>
2018-01-28 12:06:09 +0800

    dict

---
+++ 0128_p05_dict_2.py
@@ -0,0 +1,18 @@
+Keys = 'P', 'M', 'H'
+values = 'Pikachu', 'Mickey Mouse', 'Hello kitty'
+dc = dict(zip(Keys, values))
+
+
+while True:
+    qkey = input()
+    if qkey == '-1':
+        break
+    
+    if qkey not in dc:
+        
+        print(qkey,'does not exist. Enter a new one:')
+        dc[qkey] = input()
+    else:
+        print(dc[qkey])
+
+
