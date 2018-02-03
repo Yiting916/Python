88d155a5f9f6d2c38555112bf4f234eec7bc1103
Summer <yiting.chen916@gmail.com>
2018-02-03 10:37:36 +0800

    set()

--- 0203_p02.py
+++ 0203_p02.py
@@ -1,16 +1,16 @@
 itemA = {'蘋果', '香蕉', '鳳梨', '芭樂'}
 itemB = {'鳳梨', '蘋果', '水梨', '蓮霧'}
 
-itemA.add('橘子')
-itemA.add('番茄')
-print('itemA 新增: 橘子, 番茄')
-print('itemA 刪除:',itemA.pop(), ', 蘋果')
+print('itemA 新增兩樣水果:')
+itemA.add(input())
+itemA.add(input())
+print('itemA 刪除了:',itemA.pop(), ', 蘋果')
 itemA.discard('蘋果')
 
-itemB.add('番茄')
-itemB.add('榴槤')
-print('itemB 新增: 榴槤, 番茄')
-print('itemB 刪除:',itemB.pop(), ', 蓮霧')
+print('itemB 新增兩樣水果:')
+itemB.add(input())
+itemB.add(input())
+print('itemB 刪除了:',itemB.pop(), ', 蓮霧')
 itemB.discard('蓮霧')
 
 print('所有的水果且不重覆: ', sorted(itemA | itemB))
